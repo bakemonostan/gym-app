6ab8e842b3ce1451eb76f218c4bb07a02103cfbb
bakemonostan <ehizomokhomion@gmail.com>
2022-07-02 06:05:29 -0700

    navbar complted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,36 @@
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
+import Logo from '../assets/images/Logo.png';
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
-    </div>
+    <Stack
+      direction='row'
+      sx={{ gap: { sm: '124px', xs: '40px' }, mt: { sm: '32px', xs: '20px' } }}
+      px='20px'
+    >
+      <Link to='/'>
+        <img src={Logo} alt='logo' style={{ margin: '0 20px' }} />
+      </Link>
+      <Stack direction='row' gap='40px' fontSize='24px' alignItems='flex-end'>
+        <Link
+          to='/'
+          style={{
+            textDecoration: 'none',
+            color: '#3A1212',
+            borderBottom: '3px solid #FF2625',
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href='#exercise'
+          style={{ textDecoration: 'none', color: '#3A1212' }}
+        >
+          Exercise
+        </a>
+      </Stack>
+    </Stack>
   );
 };
 export default Navbar;
